9b1a3dc921f4d2c60c85cdfa2dd7ad902dfd9a4e
Gaarli <gabrieldearaujolima162@gmail.com>
2026-09-10 13:07:42 -0300

    linked list cyclet

---
+++ linked-list/141. Linked List Cycle.py
@@ -0,0 +1,45 @@
+"""
+# Solução usando hashset O(n) time e O(n) space
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        hs = set()
+
+        curr = head
+        while curr:
+            if curr in hs:
+                return True
+            else:
+                hs.add(curr)
+            curr = curr.next
+        return False
+
+"""
+
+# Solução usando 2 ponteiros O(n) time e O(1) space
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+# A ideia é que uma hora os ponteiros vão se encontrar
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow==fast:
+                return True
+
+        return False
